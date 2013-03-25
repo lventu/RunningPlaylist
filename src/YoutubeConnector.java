ef8885a7726989d5726eb147ee9160a67c1096df
Luca Venturini <dr.wen289@gmail.com>
2013-03-26 00:39:34 +0100

    Connection optimization. Class re-factored.

--- src/YoutubeConnector.java
+++ src/YoutubeConnector.java
@@ -6,26 +6,37 @@ import com.google.gdata.data.youtube.VideoEntry;
 import com.google.gdata.data.youtube.VideoFeed;
 import com.google.gdata.data.youtube.YouTubeMediaGroup;
 
-/**
+/** Class to query youtube site
  * @author luca
  *
  */
 public class YoutubeConnector {
 	
-	public YoutubeConnector() throws Exception{
-		
-	}
+	private YouTubeQuery query;
+	private YouTubeService service;
 	
-	public String getYoutubeVideoID(String author, String title) throws Exception {
-		YouTubeQuery query = new YouTubeQuery(new URL("http://gdata.youtube.com/feeds/api/videos"));
+	/**Constructor
+	 * @throws Exception on creation failure
+	 */
+	public YoutubeConnector() throws Exception{
+		query = new YouTubeQuery(new URL("http://gdata.youtube.com/feeds/api/videos"));
 		query.setOrderBy(YouTubeQuery.OrderBy.RELEVANCE);
 		query.setSafeSearch(YouTubeQuery.SafeSearch.NONE);
+		query.setStrict(true);
+		service = new YouTubeService("luca");		
+	}
+	
+	/**Function to get video ID from a artist/title of a track
+	 * @param author song author
+	 * @param title song title
+	 * @return video ID
+	 * @throws Exception on query execution error
+	 */
+	public String getYoutubeVideoID(String author, String title) throws Exception {
 		query.setFullTextQuery(author+" "+title);
-		YouTubeService service = new YouTubeService("luca");
 		VideoFeed videoFeed = service.query(query,VideoFeed.class);
 		VideoEntry list= videoFeed.getEntries().get(0);
 		YouTubeMediaGroup media = list.getMediaGroup();
-		System.out.println(title + "-->\t"+ media.getVideoId());
 		return media.getVideoId();
 	}
 	
