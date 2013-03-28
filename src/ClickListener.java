a4c56c1b995d4e6874eedc2b6faf700606d90e71
Luca Venturini <dr.wen289@gmail.com>
2013-03-29 00:40:34 +0100

    Added integration with dowloader.

--- src/ClickListener.java
+++ src/ClickListener.java
@@ -3,21 +3,28 @@ import java.awt.event.ActionListener;
 import java.util.List;
 
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 
 import song.SongItem;
 
-
-/** Class that implement the click listener for the button
+/**
+ * Class that implement the click listener for the button
+ * 
  * @author luca
  */
 public class ClickListener implements ActionListener {
-	
+
 	YoutubeConnector connector;
-	
+	private TableSelectionListener selectionListener;
+
 	/**
 	 * Constructor
+	 * 
+	 * @param selectionListener
 	 */
-	public ClickListener() {
+	public ClickListener(TableSelectionListener selectionListener) {
+		this.selectionListener = selectionListener;
+		selectionListener.saveRowSelected();
 		try {
 			connector = new YoutubeConnector();
 		} catch (Exception e) {
@@ -28,29 +35,48 @@ public class ClickListener implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent asEvent) {
-		String buttonName = ((JButton) asEvent.getSource()).getName();
-		switch(buttonName){
+		JButton button = ((JButton) asEvent.getSource());
+		String buttonName = button.getName();
+		switch (buttonName) {
 		case "OK":
-			if (!TableSelectionListener.selectedSong.isEmpty()){
-				List<SongItem> list = TableSelectionListener.selectedSong;
-				System.out.println("#"+TableSelectionListener.selectedSong.size()+" Songs Selected");
-				String asVideoId = null;
-				for (SongItem song : list) {
-					try {
-						asVideoId = connector.getYoutubeVideoID(song.getSongArtist(), song.getSongTitle());
-						System.out.println(song.getSongTitle() + "\t\t"+ asVideoId);
-					} catch (Exception e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
+			if (!selectionListener.selectedSong.isEmpty()) {
+				List<SongItem> list = selectionListener.selectedSong;
+				int choiceReturn = JOptionPane.showConfirmDialog(
+						button.getParent(),
+						"Conferma scaricamento di " + list.size() + " video",
+						"Attenzione", JOptionPane.YES_NO_OPTION);
+
+				switch (choiceReturn) {
+				case JOptionPane.OK_OPTION:
+					System.out.println("OPERAZIONE CONFERMATA");
+					String lsVideoId = null;
+					for (SongItem song : list) {
+						try {
+							lsVideoId = connector.getYoutubeVideoID(
+									song.getSongArtist(), song.getSongTitle());
+							System.out.println(song.getSongTitle() + "\t\t"
+									+ lsVideoId);
+							YTDWrapper.ytdAddVideoUrl(lsVideoId);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
 					}
+					selectionListener.freezeTableSelection();
+
+					break;
+				case JOptionPane.NO_OPTION:
+					System.out.println("OPERAZIONE CANCELLATA");
+					break;
 				}
+
 			}
+
 			break;
-		case "ANNULLA":	
-			TableSelectionListener.resetRowSelection();
+		case "ANNULLA":
+			selectionListener.resetRowSelection();
+			selectionListener.unfreezeTableSelection();
 			System.out.println("Selection Resetted");
 			break;
 		}
 	}
-
 }
